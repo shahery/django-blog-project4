941f1fc5a1afb4017ed271f412e0caf10f5cb43c
shahery <shaheryar924@gmail.com>
2022-07-06 10:59:46 +0000

    Add the python testing for forms as test_forms.py file

--- blog/test_models.py
+++ blog/test_models.py
@@ -4,12 +4,15 @@
 
 # class TestBlogModels(TestCase):
 
-    # def test_model_str(self):
-    #     title = Post.objects.create(title=200)
-    #     content = Post.objects.create(content='This is something')
-    #     self.assertEqual(srt(title), 'Django Testing')
+#     def test_model_Post(self):
+#         title = Post.objects.create(title="Django Testing")
+#         content = Post.objects.create(content='This is something')
+#         self.assertEqual(srt(title), 'Django Testing')
 
-    # def test_model_str1(self):
+    # def test_model_Comment(self):
+    #     self.assertEqual(str(self.title), 'Django')
+
+    # def test_model_Category(self):
     #     self.assertEqual(str(self.title), 'Django')
 
     # def test_post_string_method_returns_name(self):
