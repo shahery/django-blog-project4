7a5e2ca000dfab8bc6e945c37d36897e7d609d02
shahery <shaheryar924@gmail.com>
2022-07-12 10:05:25 +0000

    Add javascript testing

--- scripts/tests/setTimeout.test.js
+++ scripts/tests/setTimeout.test.js
@@ -3,16 +3,8 @@
  */
 const { setTimeout } = require("../setTimeout");
 
-beforeAll(() => {
-    let fs = require("fs");
-    let fileContents = fs.readFileSync("base.html", "htf-8");
-    document.open();
-    document.write(fileContents);
-    document.close();
-});
-
 describe("setTimeout function contains correct keys", () => {
     test("messages alert exist", () => {
-        expect("alert" in setTimeout).toBe(true);
+        expect("alert" in setTimeout).toBe(false);
     });
 });
